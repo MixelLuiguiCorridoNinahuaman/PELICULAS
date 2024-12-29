2abdbc311190057d1d8f2f20f0305339a3b27c5c
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2024-12-30 00:57:12 +0530

    created nav bar, blog post cards, footer, search dialog box, share dialog box

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
+import Footer from '../footer/Footer'
 
-function Layout() {
-  return (
-    <div>Layout</div>
-  )
+function Layout({ children }) {
+    return (
+        <div>
+            {/* Navbar  */}
+            <Navbar />
+
+            {/* main Content  */}
+            <div className="content min-h-screen">
+                {children}
+            </div>
+
+            {/* Footer  */}
+            <Footer />
+        </div>
+    )
 }
 
 export default Layout
