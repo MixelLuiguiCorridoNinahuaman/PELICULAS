240bdf94eaad983c57e7dc718249c1d8dddb7397
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2024-12-30 02:51:34 +0530

    setup firestone and firebase storage

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
@@ -15,5 +18,10 @@ const firebaseConfig = {
   };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore(app);
+const auth = getAuth(app)
+const storage = getStorage(app);
 const analytics = getAnalytics(app);
+export { fireDB, auth, storage };
