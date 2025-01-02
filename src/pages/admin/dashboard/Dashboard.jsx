d44c95006b1b70c7837af84e664fba155279f3fe
AchiraNadeeshan <hrachiranadeeshan@gmail.com>
2025-01-03 01:41:22 +0530

    Updated admin dashboard

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -36,19 +36,19 @@ function Dashboard() {
                             className='text-center font-bold text-2xl mb-2'
                             style={{ color: mode === 'dark' ? 'white' : 'black' }}
                         >
-                            Kamal Nayan Upadhyay
+                            Achira Nadeeshan Dilhara
                         </h1>
 
-                        <h2
+                        {/* <h2
                             style={{ color: mode === 'dark' ? 'white' : 'black' }} className="font-semibold">
                             Software Developer
-                        </h2>
+                        </h2> */}
                         <h2
-                            style={{ color: mode === 'dark' ? 'white' : 'black' }} className="font-semibold">knupadhyay784@gmail.com
+                            style={{ color: mode === 'dark' ? 'white' : 'black' }} className="font-semibold">hrachiranadeeshan@gmail.com
                         </h2>
                         <h2
                             style={{ color: mode === 'dark' ? 'white' : 'black' }} className="font-semibold">
-                            <span>Total Blog : </span>  15
+                            {/* <span>Total Blog : </span>  15 */}
                         </h2>
                         <div className=" flex gap-2 mt-2">
                             <Link to={'/createblog'}>
